4bf21576db8706ef703bdb9a0ef7349de608e7ea
seungit <minsg3669@gmail.com>
2023-04-05 13:36:06 +0900

    feat : project setting, mysql connect

---
+++ src/test/java/com/example/webnovelclonecoding/WebNovelCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webnovelclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebNovelCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
